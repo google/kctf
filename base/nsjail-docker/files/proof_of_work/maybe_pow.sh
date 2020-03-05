b0dfd96b2fea92c815537e13d5d4464bc41fe627
Stephen Roettger <sroettger@google.com>
2020-03-05 17:02:03 +0100

    always run pow.py

--- base/nsjail-docker/files/proof_of_work/maybe_pow.sh
+++ base/nsjail-docker/files/proof_of_work/maybe_pow.sh
@@ -12,13 +12,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 if [ -f /config/pow ]; then
   POW="$(cat /config/pow)"
-  if [ ${POW} != 0 ]; then
-    if ! /usr/bin/pow.py ask "${POW}"; then
-      echo 'pow fail'
-      exit 1
-    fi
+  if ! /usr/bin/pow.py ask "${POW}"; then
+    echo 'pow fail'
+    exit 1
   fi
 fi
 
