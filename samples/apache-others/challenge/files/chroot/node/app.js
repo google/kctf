fc764c7211e62a17f25ebde2bf5c8058af77a039
evn <evn@google.com>
2020-03-08 18:51:26 +0100

    remove unecessary files and add healthcheck

--- samples/apache-others/challenge/files/chroot/node/app.js
+++ samples/apache-others/challenge/files/chroot/node/app.js
@@ -6,9 +6,10 @@ const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end(req.url.split('').reverse().join(''));
 });
 
 server.listen(port, hostname, () => {
+  // Send something to STDOUT to signal we are ready to accept connections
   console.log(`Server running at http://${hostname}:${port}/`);
 });
--- samples/apache-others/challenge/files/chroot/node/run.sh
+++ samples/apache-others/challenge/files/chroot/node/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-mkfifo /tmp/node
-node /home/user/chroot/node/app.js >/tmp/node &
-head -n 1 /tmp/node >/dev/null
+# Create a pipe
+mkfifo /tmp/pipe
+
+# Start node web server
+node /home/user/chroot/node/app.js >/tmp/pipe &
+
+# This will wait for /tmp/pipe to be written to
+head -n 1 /tmp/pipe >/dev/null
+
+# Proxy stdin/stdout to web server
 socat - TCP:127.0.0.1:8080
