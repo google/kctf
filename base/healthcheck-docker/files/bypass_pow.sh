d38a244899b3859120c7d143db6bcc5d4cb239dc
Stephen Roettger <sroettger@google.com>
2020-03-10 04:06:30 +0100

    no newline when signing challenge

--- base/healthcheck-docker/files/bypass_pow.sh
+++ base/healthcheck-docker/files/bypass_pow.sh
@@ -4,6 +4,6 @@ CHALLENGE="$1"
 
 KEY="/pow-bypass/pow-bypass-key.pem"
 
-SIG=$(echo "${CHALLENGE}" | openssl dgst -SHA256 -hex -sign "${KEY}" - | awk '{print $2}')
+SIG=$(echo -n "${CHALLENGE}" | openssl dgst -SHA256 -hex -sign "${KEY}" - | awk '{print $2}')
 
 echo -n "b.${SIG}"
