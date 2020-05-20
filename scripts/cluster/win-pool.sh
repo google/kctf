8bddc05f024ebca9ed22ea84135e9b1320fe7345
Stephen Roettger <sroettger@google.com>
2020-05-20 11:29:20 +0200

    add command line parameters to win-pool script

--- scripts/cluster/win-pool.sh
+++ scripts/cluster/win-pool.sh
@@ -22,8 +22,57 @@ MACHINE_TYPE="n2-standard-4"
 MIN_NODES="1"
 MAX_NODES="2"
 NUM_NODES="1"
+IMAGE_TYPE="WINDOWS_SAC"
+POOL_NAME="win"
+#IMAGE_TYPE="WINDOWS_LTSC"
 
-gcloud container node-pools create "win" \
+function usage {
+    echo "$(basename $0) [args]" >&2
+    echo -e "\t--machine-type\tMachine type to use for the windows pool (default: \"${MACHINE_TYPE}\")" >&2
+    echo -e "\t--min-nodes\tMinimum number of nodes" >&2
+    echo -e "\t--max-nodes\tMaximum number of nodes" >&2
+    echo -e "\t--num-nodes\tInitial number of nodes" >&2
+    echo -e "\t--image-type\tWindows image type (e.g. WINDOWS_SAC or WINDOWS_LTSC)" >&2
+    echo -e "\t--pool-name\tName of the node pool (default: \"${POOL_NAME}\")" >&2
+    exit 1
+}
+
+while :; do
+    if [[ -z "${1:-}" ]]; then
+        break
+    fi
+    if [[ -z "${2:-}" ]]; then
+        echo "Missing argument after \"$1\"." >&2
+        usage
+    fi
+    case $1 in
+        --machine-type)
+            MACHINE_TYPE=$2
+            ;;
+        --min-nodes)
+            MIN_NODES=$2
+            ;;
+        --max-nodes)
+            MAX_NODES=$2
+            ;;
+        --num-nodes)
+            NUM_NODES=$2
+            ;;
+        --image-type)
+            IMAGE_TYPE=$2
+            ;;
+        --pool-name)
+            POOL_NAME=$2
+            ;;
+        *)
+            echo "Unrecognized argument \"$1\"." >&2
+            usage
+            ;;
+    esac
+    shift
+done
+
+gcloud container node-pools create "${POOL_NAME}" \
   --cluster="${CLUSTER_NAME}" \
   --machine-type="${MACHINE_TYPE}" \
   --enable-autorepair \
@@ -33,4 +82,4 @@ gcloud container node-pools create "win" \
   --min-nodes="${MIN_NODES}" \
   --max-nodes="${MAX_NODES}" \
   --no-enable-autoupgrade \
-  --image-type=WINDOWS_SAC
+  --image-type="${IMAGE_TYPE}"
