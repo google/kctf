2a631985aa34f27a837f1271b51346234bb56448
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2020-06-30 16:15:36 +0200

    Assign stdout to a variable instead of devnull
    /dev/null errors out now for some reason

--- samples/apache-others/challenge/image/chroot/node/run.sh
+++ samples/apache-others/challenge/image/chroot/node/run.sh
@@ -7,7 +7,7 @@ mkfifo /tmp/pipe
 node /home/user/chroot/node/app.js >/tmp/pipe &
 
 # This will wait for /tmp/pipe to be written to
-head -n 1 /tmp/pipe >/dev/null
+status=$(head -n 1 /tmp/pipe)
 
 # Proxy stdin/stdout to web server
 socat - TCP:127.0.0.1:8080
