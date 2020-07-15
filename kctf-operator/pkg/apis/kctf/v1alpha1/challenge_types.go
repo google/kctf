b15aaeb643425ff4e701e43dcfdcf99669e9c539
Alicia Machado <aliciafm@google.com>
2020-07-15 16:20:47 +0000

    Adding operator files and initial CRD generator

---
+++ kctf-operator/pkg/apis/kctf/v1alpha1/challenge_types.go
@@ -0,0 +1,89 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+type PortSpec struct {
+	// TODO: port shouldn't be an obligatory field
+	Port       int    `json:"port"`
+	TargetPort int    `json:"targetport"`
+	Protocol   string `json:"protocol"`
+}
+
+type NetworkSpec struct {
+	Public bool       `json:"public"`
+	Dns    bool       `json:"dns"`
+	Ports  []PortSpec `json:"ports"`
+}
+
+type HealthcheckSpec struct {
+	Enabled bool `json:"healthcheckEnabled"`
+	// TODO: container should be always healthcheck, i guess
+	Container string `json:"healthcheckContainer"`
+}
+
+type AutoscalingSpec struct {
+	Enabled                        bool `json:"autoscalingEnabled"`
+	MinReplicas                    int  `json:"minreplicas"`
+	MaxReplicas                    int  `json:"maxreplicas"`
+	TargetCPUUtilizationPercentage int  `json:"targetcpuutilizationpercentage"`
+}
+
+type DeploymentSpec struct {
+	// TODO: see how to use persistent volumes and etc ...
+
+}
+
+// ChallengeSpec defines the desired state of Challenge
+type ChallengeSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	// TODO: add other fields
+	// TODO: add maximum, minimum and default value
+	// TODO: discover how to include disk stuff
+	// TODO: ready in status or in spec?
+	ImageTemplate        string          `json:"imagetemplate"`
+	PowDifficultySeconds int             `json:"powdifficultyseconds"`
+	Network              NetworkSpec     `json:"network"`
+	Healthcheck          HealthcheckSpec `json:"healthcheck"`
+	Autoscaling          AutoscalingSpec `json:"autoscaling"`
+	// Deployment DeploymentSpec `json:"deployment"`
+}
+
+// ChallengeStatus defines the observed state of Challenge
+type ChallengeStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	// TODO: implement status for the challenges like READY and etc..
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Challenge is the Schema for the challenges API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=challenges,scope=Namespaced
+type Challenge struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ChallengeSpec   `json:"spec,omitempty"`
+	Status ChallengeStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ChallengeList contains a list of Challenge
+type ChallengeList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Challenge `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Challenge{}, &ChallengeList{})
+}
---
+++ kctf-operator/pkg/apis/kctf/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the kctf v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=kctf.dev
+package v1alpha1
