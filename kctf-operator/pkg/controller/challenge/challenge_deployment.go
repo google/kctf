b59e8dcd7db02a0aab17b9ff1fd99ebcaa218d92
Alicia Machado <aliciafm@google.com>
2020-08-12 15:15:37 +0000

    Added healthcheck deployment and added PodTemplate volume mounts to the deployment

--- kctf-operator/pkg/controller/challenge/challenge_deployment.go
+++ kctf-operator/pkg/controller/challenge/challenge_deployment.go
@@ -10,6 +10,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	resource "k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	intstr "k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
@@ -27,16 +28,84 @@ func ContainerPorts(challenge *kctfv1alpha1.Challenge) []corev1.ContainerPort {
 	return ports
 }
 
+// Adds volume mounts from podtemplate
+// It would be interesting that the user can put some features in the pod template
+// and they are kept in the actual deployment, but a StrategicMerge as in kustomization
+// wasn't found so we would need to define our own logic for merging
+func MergeWithPodTemplate(challenge *kctfv1alpha1.Challenge, deployment *appsv1.Deployment) {
+	deployment.Spec.Template.Spec.Containers[0].VolumeMounts =
+		append(deployment.Spec.Template.Spec.Containers[0].VolumeMounts,
+			challenge.Spec.PodTemplate.Template.Spec.Containers[0].VolumeMounts...)
+}
+
 // Deployment with Healthcheck
 func (r *ReconcileChallenge) deploymentWithHealthcheck(challenge *kctfv1alpha1.Challenge) *appsv1.Deployment {
-	//TODO
-	dep := &appsv1.Deployment{}
+	dep := r.deployment(challenge)
+
+	// Get the container with the challenge and add healthcheck configurations
+	dep.Spec.Template.Spec.Containers[0].LivenessProbe = &corev1.Probe{
+		FailureThreshold: 2,
+		Handler: corev1.Handler{
+			HTTPGet: &corev1.HTTPGetAction{
+				Path: "/healthz",
+				Port: intstr.FromInt(45281),
+			},
+		},
+		InitialDelaySeconds: 45,
+		TimeoutSeconds:      3,
+		PeriodSeconds:       30,
+	}
+
+	dep.Spec.Template.Spec.Containers[0].ReadinessProbe = &corev1.Probe{
+		Handler: corev1.Handler{
+			HTTPGet: &corev1.HTTPGetAction{
+				Path: "/healthz",
+				Port: intstr.FromInt(45281),
+			},
+		},
+		InitialDelaySeconds: 5,
+		TimeoutSeconds:      3,
+		PeriodSeconds:       5,
+	}
+
+	healthcheckContainer := corev1.Container{
+		Name:    "healthcheck",
+		Image:   "healthcheck",
+		Command: []string{},
+		Resources: corev1.ResourceRequirements{
+			Limits: corev1.ResourceList{
+				"cpu": *resource.NewMilliQuantity(1000, resource.DecimalSI),
+			},
+			Requests: corev1.ResourceList{
+				"cpu": *resource.NewMilliQuantity(50, resource.DecimalSI),
+			},
+		},
+		// Uncomment when start testing with real challenges
+		VolumeMounts: []corev1.VolumeMount{{
+			Name:      "pow-bypass",
+			ReadOnly:  true,
+			MountPath: "/pow-bypass",
+		}},
+	}
+
+	dep.Spec.Template.Spec.Containers = append(dep.Spec.Template.Spec.Containers, healthcheckContainer)
+
+	healthcheckVolume := corev1.Volume{
+		Name: "pow-bypass",
+		VolumeSource: corev1.VolumeSource{
+			Secret: &corev1.SecretVolumeSource{
+				SecretName: "pow-bypass",
+			},
+		},
+	}
+
+	dep.Spec.Template.Spec.Volumes = append(dep.Spec.Template.Spec.Volumes, healthcheckVolume)
+
 	return dep
 }
 
 // Deployment without Healthcheck
-// TODO: Connect the podTemplate passed and the deployment
-func (r *ReconcileChallenge) deploymentWithoutHealthcheck(challenge *kctfv1alpha1.Challenge) *appsv1.Deployment {
+func (r *ReconcileChallenge) deployment(challenge *kctfv1alpha1.Challenge) *appsv1.Deployment {
 	ls := labelsForChallenge(challenge.Name)
 	var replicas int32 = 1
 	var readOnlyRootFilesystem = true
@@ -70,7 +139,7 @@ func (r *ReconcileChallenge) deploymentWithoutHealthcheck(challenge *kctfv1alpha
 							},
 							ReadOnlyRootFilesystem: &readOnlyRootFilesystem,
 						},
-						// TODO: "command" is present in the original deployment file but it's empty, should we add set it as nil?
+						Command: []string{},
 						Resources: corev1.ResourceRequirements{
 							Limits: corev1.ResourceList{
 								"cpu": *resource.NewMilliQuantity(900, resource.DecimalSI),
@@ -117,7 +186,10 @@ func (r *ReconcileChallenge) deploymentWithoutHealthcheck(challenge *kctfv1alpha
 
 	// Set container ports based on the ports that were passed
 	dep.Spec.Template.Spec.Containers[0].Ports = ContainerPorts(challenge)
-
+	// Merges with Pod Template
+	if challenge.Spec.PodTemplate != nil {
+		MergeWithPodTemplate(challenge, dep)
+	}
 	// Set Challenge instance as the owner and controller
 	controllerutil.SetControllerReference(challenge, dep, r.scheme)
 	return dep
@@ -134,7 +206,7 @@ func (r *ReconcileChallenge) deploymentForChallenge(challenge *kctfv1alpha1.Chal
 	if challenge.Spec.Healthcheck.Enabled == true {
 		return r.deploymentWithHealthcheck(challenge)
 	} else {
-		return r.deploymentWithoutHealthcheck(challenge)
+		return r.deployment(challenge)
 	}
 }
 
