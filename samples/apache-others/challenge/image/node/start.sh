4407215edffde363002adb9964af6ce167b234ba
sroettger <sroettger@google.com>
2020-12-05 08:04:41 +0100

    Deploy all tasks in test (#154)
    * deploy all tasks in test
    
    * include nsjail base conf in apache-others
    
    * add required healthcheck option to challenge skeleton
    
    * fix namespace != challenge name
    
    * read the challenge name from the yaml
    
    * update puppeteer dependencies
    
    * Automated commit: update images.
    
    Co-authored-by: Stephen Roettger <stephen.roettger@gmail.com>

--- samples/apache-others/challenge/image/node/start.sh
+++ samples/apache-others/challenge/image/node/start.sh
@@ -2,5 +2,5 @@
 
 mount -t tmpfs none /var/log/apache2
 
-drop_privs nsjail --config /home/user/nsjail.cfg --port 8081 -- \
+drop_privs nsjail --config /kctf/nsjail_base.cfg --config /home/user/nsjail.cfg --port 8081 -- \
   /home/user/chroot/node/run.sh &
