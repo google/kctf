7e433f655f22825f9621e54bbcac26d7eb56b2df
Stephen Roettger <stephen.roettger@gmail.com>
2020-12-07 14:56:08 +0100

    create network policy on challenge create

--- kctf-operator/pkg/resources/initializer.go
+++ kctf-operator/pkg/resources/initializer.go
@@ -17,15 +17,13 @@ var log logr.Logger = logf.Log.WithName("cmd")
 func InitializeOperator(client *client.Client) error {
 	// Creates the objects that enable the DNS, external DNS and etc
 
-	objectFunctions := []func() runtime.Object{NewApparmorProfiles, NewAllowDns, NewServiceAccountGcsFuseSa,
+	objectFunctions := []func() runtime.Object{NewApparmorProfiles, NewServiceAccountGcsFuseSa,
 		NewServiceAccountExternalDnsSa, NewExternalDnsClusterRole, NewExternalDnsClusterRoleBinding,
-		NewExternalDnsDeployment, NewDaemonSetCtf, NewDaemonSetGcsFuse, NewNetworkPolicyBlockInternal,
-		NewNetworkPolicyKubeSystem, NewSecretPowBypass, NewSecretPowBypassPub}
+		NewExternalDnsDeployment, NewDaemonSetCtf, NewDaemonSetGcsFuse, NewSecretPowBypass, NewSecretPowBypassPub}
 
 	names := []string{"Apparmor Profiles", "Service Account Gcs Fuse", "Service Account External DNS",
-		"Allow DNS", "External DNS Cluster Role",
-		"External DNS Cluster Role Binding", "External DNS Deployment", "Daemon Set Ctf", "Daemon Set Gcs Fuse",
-		"Network Policy Block Internal", "Network Policy Kube System", "Secret for PowBypass", "Secret for PowBypassPub"}
+		"External DNS Cluster Role", "External DNS Cluster Role Binding", "External DNS Deployment",
+		"Daemon Set Ctf", "Daemon Set Gcs Fuse", "Secret for PowBypass", "Secret for PowBypassPub"}
 
 	for i, newObject := range objectFunctions {
 
