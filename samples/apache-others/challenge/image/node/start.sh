35065853a212c6c5a1155885f567faf7441fa6c8
Stephen Roettger <stephen.roettger@gmail.com>
2020-12-16 15:45:30 +0100

    move tmp mounts to challenge.yaml and run as root

--- samples/apache-others/challenge/image/node/start.sh
+++ samples/apache-others/challenge/image/node/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mount -t tmpfs none /var/log/apache2
-
-drop_privs nsjail --config /kctf/nsjail_base.cfg --config /home/user/nsjail.cfg --port 8081 -- \
-  /home/user/chroot/node/run.sh &
+setpriv --init-groups --reset-env --reuid user --regid user --inh-caps=-all -- \
+  nsjail --config /kctf/nsjail_base.cfg --config /home/user/nsjail.cfg --port 8081 -- \
+    /home/user/chroot/node/run.sh &
