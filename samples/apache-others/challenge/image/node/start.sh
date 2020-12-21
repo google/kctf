47e6de51a9e5b270990e5155159d451247432abb
sroettger <sroettger@google.com>
2020-12-21 07:25:20 +0100

    Revert "Run as uid 1000 by default with ProcMount: Unmasked (#169)" (#172)
    * Revert "Run as uid 1000 by default with ProcMount: Unmasked (#169)"
    
    This doesn't seem to work even though I did test it manually and the
    workflow tests passed as well.
    Not sure exactly why, but reverting it for now.
    
    * Automated commit: update images.
    
    Co-authored-by: Stephen Roettger <stephen.roettger@gmail.com>

--- samples/apache-others/challenge/image/node/start.sh
+++ samples/apache-others/challenge/image/node/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-setpriv --init-groups --reset-env --reuid user --regid user --inh-caps=-all -- \
-  nsjail --config /kctf/nsjail_base.cfg --config /home/user/nsjail.cfg --port 8081 -- \
-    /home/user/chroot/node/run.sh &
+mount -t tmpfs none /var/log/apache2
+
+drop_privs nsjail --config /kctf/nsjail_base.cfg --config /home/user/nsjail.cfg --port 8081 -- \
+  /home/user/chroot/node/run.sh &
