c43784bc7bd9aa788a41575f9203214c8163d7d4
Stephen Roettger <stephen.roettger@gmail.com>
2020-12-24 11:02:22 +0100

    s/drop_privs/kctf_drop_privs/

--- samples/apache-others/challenge/image/node/start.sh
+++ samples/apache-others/challenge/image/node/start.sh
@@ -2,5 +2,5 @@
 
 mount -t tmpfs none /var/log/apache2
 
-drop_privs nsjail --config /home/user/nsjail.cfg --port 8081 -- \
+kctf_drop_privs nsjail --config /home/user/nsjail.cfg --port 8081 -- \
   /home/user/chroot/node/run.sh &
