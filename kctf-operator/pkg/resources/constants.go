f056eb54008125de8b52ff875b648588ac0498b9
evn <evn@google.com>
2021-01-18 15:21:26 +0100

    use proper escaping

--- kctf-operator/pkg/resources/constants.go
+++ kctf-operator/pkg/resources/constants.go
@@ -5,8 +5,8 @@ package resources
 // == || These are set by automation || ==
 // .. vv ........................... vv ..
 
-const DOCKER_CERTBOT_IMAGE = "${CERTBOT_IMAGE_URL}@${CERTBOT_IMAGE_ID}"
-const DOCKER_GCSFUSE_IMAGE = "${GCSFUSE_IMAGE_URL}@${GCSFUSE_IMAGE_ID}"
+const DOCKER_CERTBOT_IMAGE = "eu.gcr.io/sdcpocs1/certbot@sha256:620f0f4430e8b4f596578759c55ba0d7fee66fbe5f00a7e90b9b707afa231922"
+const DOCKER_GCSFUSE_IMAGE = "eu.gcr.io/sdcpocs1/gcsfuse@sha256:f91aac6fec8cb273610ca497747f131cb4ca23d6de50b12f761f98564e41e639"
 
 // .. ^^ ........................... ^^ ..
 // == || These are set by automation || ==
