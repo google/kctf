0cb679d60b83ace9ff3e03364fe59930b3e37e03
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2021-01-20 11:55:57 +0100

    Pass better flags to certbot
    This changes 3 things:
     1. Remove `--break-my-certs`. This was failing for a different reason than I thought (turns out it was the quotes around `"${TEST}"`...)
     2. Allow registering for a wildcard cert without an email. Since we still always use `ManagedCertificate`, this sounds fine if things break (eg, client-incompatible changes)
     3. Reorder ifs a little bit to make required flags on top, and optional ones below.

--- docker-images/certbot/certbot.sh
+++ docker-images/certbot/certbot.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-TEST="--test-cert"
-
 if [ -z "${DOMAIN}" ]; then
-  echo Make sure the DOMAIN environment variable points to the domain.
-  exit 1
-fi
-
-if [ -z "${EMAIL}" ]; then
-  echo Make sure the EMAIL environment variable points to the email.
+  echo "Make sure the DOMAIN environment variable points to the domain."
   exit 1
 fi
 
 if [ -z "${SECRET}" ]; then
-  echo Make sure the SECRET environment variable points to the k8s secret.
+  echo "Make sure the SECRET environment variable points to the k8s secret."
   exit 1
 fi
 
 if [ -z "${PROD}" ]; then
-  echo Making a test certificate because PROD environment variable is not set.
+  echo "Making a TEST certificate because PROD environment variable is NOT set."
+  TEST="--test-cert"
 else
-  echo Making a valid certificate because PROD environment variable is set.
-  TEST="--break-my-certs"
+  echo "Making a REAL certificate because PROD environment variable is set."
+  TEST=""
+fi
+
+if [ -z "${EMAIL}" ]; then
+  echo "Registering certificate unsafely without email. Pass an EMAIL to register an account with an email address."
+  EMAIL_FLAG="--register-unsafely-without-email"
+else
+  EMAIL_FLAG="-m ${EMAIL}"
 fi
 
 function request_certificate() {
-  certbot certonly "${TEST}" --non-interactive --agree-tos -m "${EMAIL}" --dns-google -d '*.'"${DOMAIN}" --dns-google-propagation-seconds 120
+  certbot certonly ${TEST} --non-interactive --agree-tos ${EMAIL_FLAG} --dns-google -d '*.'"${DOMAIN}" --dns-google-propagation-seconds 120
 }
 
 function update_tls_secret() {
@@ -37,12 +38,12 @@ function check_tls_validity() {
 }
 
 while true; do
-  echo Waiting 2 minutes to avoid hitting rate limits
+  echo "Waiting 2 minutes to avoid hitting rate limits"
   sleep 2m
   if check_tls_validity; then
-    echo Certificate is valid for at least 30 days
+    echo "Certificate is valid for at least 30 days"
     sleep 1d
   else
-    request_certificate && update_tls_secret && echo TLS cert updated
+    request_certificate && update_tls_secret && echo "TLS cert updated"
   fi
 done
