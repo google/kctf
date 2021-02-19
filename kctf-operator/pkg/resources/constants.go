052e00174cbe63c6b836955a2c5babbb74b3a3bb
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2021-02-19 19:51:54 +0100

    Merge commands config and cluster under cluster (#226)
    * Make cluster/config zusamen
    
    * fix workflow
    
    * Automated commit: update images.
    
    * rename start-cluster to start
    
    * fix workflows;
    
    * update flag parser
    
    * remove value from flag
    
    * Automated commit: update images.
    
    * address code review comments
    
    * catch error writing file

--- kctf-operator/pkg/resources/constants.go
+++ kctf-operator/pkg/resources/constants.go
@@ -5,8 +5,8 @@ package resources
 // == || These are set by automation || ==
 // .. vv ........................... vv ..
 
-const DOCKER_CERTBOT_IMAGE = "gcr.io/kctf-docker/certbot@sha256:dcfdfe7eb04edeb3e5094d69a9d164901aa873ac52b650b543b90f853807912e"
-const DOCKER_GCSFUSE_IMAGE = "gcr.io/kctf-docker/gcsfuse@sha256:f2d17b8383e63e51aff7d9ca8429db8f0b0ab73323713d858c3258bad5f8287f"
+const DOCKER_CERTBOT_IMAGE = "gcr.io/kctf-docker/certbot@sha256:21a5d2d88c7d401c7228682f7fdb4769cb904757d283906b2fc0d0a2e8e76449"
+const DOCKER_GCSFUSE_IMAGE = "gcr.io/kctf-docker/gcsfuse@sha256:56ed7195472b5a9e2f1ad67ef1859abdeaa49b4e8661198a320959c7274a7a02"
 
 // .. ^^ ........................... ^^ ..
 // == || These are set by automation || ==
