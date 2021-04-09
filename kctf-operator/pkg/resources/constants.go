ef939b7cb55c2c4f2bb34f2656db71836f3bd805
sroettger <sroettger@google.com>
2021-04-09 12:09:00 +0200

    Two bugfixes: cert in kctf-system + web template backing dirs (#289)
    * create cert in kctf-system namespace
    
    * create backing directories for web challenge mounts
    
    * Automated commit: update images.

--- kctf-operator/pkg/resources/constants.go
+++ kctf-operator/pkg/resources/constants.go
@@ -5,7 +5,7 @@ package resources
 // == || These are set by automation || ==
 // .. vv ........................... vv ..
 
-const DOCKER_CERTBOT_IMAGE = "gcr.io/kctf-docker/certbot@sha256:4b649785c2fd9e063eb3aa36c100590e07dcb2e8f422f469a0ce814c3619847f"
+const DOCKER_CERTBOT_IMAGE = "gcr.io/kctf-docker/certbot@sha256:b4d98f119670c3f259a33262f4ca8ed3a9bdb8298e7695d83019606c36455e4b"
 const DOCKER_GCSFUSE_IMAGE = "gcr.io/kctf-docker/gcsfuse@sha256:85f4eac10e254651ab3ff531869b86c3b542d2dd9d0d1dbf8724a552b42ab970"
 
 // .. ^^ ........................... ^^ ..
