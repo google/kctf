76c231b9da9d25c63403bd024826c097c8014f0f
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2021-04-20 14:33:11 +0200

    Don't reference managed certificates (#287)
    * Don't reference managed certificates
    
    We don't use them anymore.
    
    * Automated commit: update images.
    
    * Automated commit: update images.

--- kctf-operator/pkg/resources/constants.go
+++ kctf-operator/pkg/resources/constants.go
@@ -5,7 +5,7 @@ package resources
 // == || These are set by automation || ==
 // .. vv ........................... vv ..
 
-const DOCKER_CERTBOT_IMAGE = "gcr.io/kctf-docker/certbot@sha256:b4d98f119670c3f259a33262f4ca8ed3a9bdb8298e7695d83019606c36455e4b"
+const DOCKER_CERTBOT_IMAGE = "gcr.io/kctf-docker/certbot@sha256:ee81493fb7544abf243a5bd5007f040cc821cacf978a516d4b699adac497dc7a"
 const DOCKER_GCSFUSE_IMAGE = "gcr.io/kctf-docker/gcsfuse@sha256:85f4eac10e254651ab3ff531869b86c3b542d2dd9d0d1dbf8724a552b42ab970"
 
 // .. ^^ ........................... ^^ ..
