4eed77efea4349df929c3884a7da764df2aff424
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2021-07-09 16:29:19 +0200

    upgrade gcsfuse to latest (#325)
    * upgrade gcsfuse to latest
    
    this is to obtain better logs that were recently added
    
    * Update Dockerfile
    
    * Automated commit: update images.

--- kctf-operator/pkg/resources/constants.go
+++ kctf-operator/pkg/resources/constants.go
@@ -6,7 +6,7 @@ package resources
 // .. vv ........................... vv ..
 
 const DOCKER_CERTBOT_IMAGE = "gcr.io/kctf-docker/certbot@sha256:b300d156df46be2615822f4f086b94a19f5101690948a9135be57aadd501ccf6"
-const DOCKER_GCSFUSE_IMAGE = "gcr.io/kctf-docker/gcsfuse@sha256:2f10b98e1a011e3eebb9470d486033cc88d2b8c5cdac425b48b40866861d5194"
+const DOCKER_GCSFUSE_IMAGE = "gcr.io/kctf-docker/gcsfuse@sha256:fecd90f412a624b36e76a15c5dfb6648f357dc9cc523b77fa58ad97b3d37a5c9"
 
 // .. ^^ ........................... ^^ ..
 // == || These are set by automation || ==
