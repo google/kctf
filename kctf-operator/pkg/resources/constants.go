f4093ac9b863b721de85bec6044830d5794e28c2
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2021-07-09 21:40:52 +0200

    fix backwards-incompatible change on gcsfuse (#328)
    * fix backwards-incompatible change on gcsfuse
    
    * Automated commit: update images.
    
    * Update Dockerfile
    
    allow caching the type (dir vs file) so that things are a bit faster. this creates weird race conditions (file vs. dir) but that should be fine (tm)
    
    * dummy commit
    
    * Automated commit: update images.

--- kctf-operator/pkg/resources/constants.go
+++ kctf-operator/pkg/resources/constants.go
@@ -1,14 +1,14 @@
 package resources
 
-// <AUTOMATICALLY GENERATED>
+//        <AUTOMATICALLY GENERATED>
 // =======================================
 // == || These are set by automation || ==
 // .. vv ........................... vv ..
 
 const DOCKER_CERTBOT_IMAGE = "gcr.io/kctf-docker/certbot@sha256:82be4c84fad2a11a6b92aa472cab2dd7018178ab89f27d4d77f46997ff539229"
-const DOCKER_GCSFUSE_IMAGE = "gcr.io/kctf-docker/gcsfuse@sha256:560897fba48dffba06e3a03c0b959bebc5e8bcb01d51e71bce75e05d0ad71119"
+const DOCKER_GCSFUSE_IMAGE = "gcr.io/kctf-docker/gcsfuse@sha256:b3e780cd860d3566bd91e6cc5a39e55377a33c22ff521b393f14947852967831"
 
 // .. ^^ ........................... ^^ ..
 // == || These are set by automation || ==
 // =======================================
-// </AUTOMATICALLY GENERATED>
+//       </AUTOMATICALLY GENERATED>
