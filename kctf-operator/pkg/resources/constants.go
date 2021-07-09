472b305eee353c56d86ee7033c0b11c3b2485ffd
GitHub Action <sroettger@google.com>
2021-07-09 15:51:06 +0000

    Automated commit: update images.

--- kctf-operator/pkg/resources/constants.go
+++ kctf-operator/pkg/resources/constants.go
@@ -5,8 +5,8 @@ package resources
 // == || These are set by automation || ==
 // .. vv ........................... vv ..
 
-const DOCKER_CERTBOT_IMAGE = "gcr.io/kctf-docker/certbot@sha256:b300d156df46be2615822f4f086b94a19f5101690948a9135be57aadd501ccf6"
-const DOCKER_GCSFUSE_IMAGE = "gcr.io/kctf-docker/gcsfuse@sha256:fecd90f412a624b36e76a15c5dfb6648f357dc9cc523b77fa58ad97b3d37a5c9"
+const DOCKER_CERTBOT_IMAGE = "gcr.io/kctf-docker/certbot@sha256:82be4c84fad2a11a6b92aa472cab2dd7018178ab89f27d4d77f46997ff539229"
+const DOCKER_GCSFUSE_IMAGE = "gcr.io/kctf-docker/gcsfuse@sha256:560897fba48dffba06e3a03c0b959bebc5e8bcb01d51e71bce75e05d0ad71119"
 
 // .. ^^ ........................... ^^ ..
 // == || These are set by automation || ==
