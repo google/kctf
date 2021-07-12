1f252c6aaf66a266d2babbf8fe70c747b9628f5e
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2021-07-12 12:36:17 +0200

    Update configmap.go

--- kctf-operator/pkg/controller/challenge/pow/configmap.go
+++ kctf-operator/pkg/controller/challenge/pow/configmap.go
@@ -11,7 +11,7 @@ import (
 // Generates the configmap that contains the how difficult should be the proof of work
 func generate(challenge *kctfv1.Challenge) *corev1.ConfigMap {
 	data := map[string]string{
-		"pow.conf": strconv.Itoa(challenge.Spec.PowDifficultySeconds * 133) + "\n",
+		"pow.conf": strconv.Itoa(challenge.Spec.PowDifficultySeconds * 1337) + "\n",
 	}
 	configmap := &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
