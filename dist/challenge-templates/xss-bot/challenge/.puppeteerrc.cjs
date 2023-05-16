de2bf3f283cbd4400bd37026ce8209bc6598d940
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2023-05-16 22:55:02 +0200

    we need to change the location from /root to /home/user https://pptr.dev/guides/configuration

---
+++ dist/challenge-templates/xss-bot/challenge/.puppeteerrc.cjs
@@ -0,0 +1,7 @@
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+    // Changes the cache location for Puppeteer.
+    cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+};
