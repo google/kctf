aac06484a89b1296e766a35bd1d66bc855080bef
GitHub Action <bartoszstebel@gmail.com>
2025-04-06 23:28:30 +0000

    Automated commit: update images.

--- kctf-operator/resources/constants.go
+++ kctf-operator/resources/constants.go
@@ -5,8 +5,8 @@ package resources
 // == || These are set by automation || ==
 // .. vv ........................... vv ..
 
-const DOCKER_CERTBOT_IMAGE = "gcr.io/kctf-docker/certbot@sha256:fdf7b50056c99c7e2b4e16ce1fbbf046334ca73bb3bc067f56e51e06720edf2d"
-const DOCKER_GCSFUSE_IMAGE = "gcr.io/kctf-docker/gcsfuse@sha256:cf43a4f5c68438cac676988fb9c6d144766252934d482b9532268e4d001a5ee9"
+const DOCKER_CERTBOT_IMAGE = "gcr.io/kctf-docker/certbot@sha256:86f883af58f630babd55a20ae7d89974d9cc35608c25cb405fe3faf90d0ceef0"
+const DOCKER_GCSFUSE_IMAGE = "gcr.io/kctf-docker/gcsfuse@sha256:6cb70b79588dc30e4ea64d85f2d0d1aff928d9002b75b697ce50009e8b88b208"
 
 // .. ^^ ........................... ^^ ..
 // == || These are set by automation || ==
