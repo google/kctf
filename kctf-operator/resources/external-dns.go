b6961fedd414c1de6dc3acbd69559ca15d039eaa
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2025-09-12 15:43:27 +0200

    Revert "namespace kctf zones by cluster name (#381)"
    This reverts commit c18a6bdd50255c7a2266861f10d3a45bd2d1886e.

--- kctf-operator/resources/external-dns.go
+++ kctf-operator/resources/external-dns.go
@@ -86,20 +86,10 @@ func NewExternalDnsDeployment() client.Object {
 										Key: "DOMAIN_NAME",
 									},
 								},
-							}, {
-								Name: "CLUSTER_NAME",
-								ValueFrom: &corev1.EnvVarSource{
-									ConfigMapKeyRef: &corev1.ConfigMapKeySelector{
-										LocalObjectReference: corev1.LocalObjectReference{
-											Name: "external-dns",
-										},
-										Key: "CLUSTER_NAME",
-									},
-								},
-							}, },
+							}},
 							Args: []string{"--log-level=debug", "--source=service", "--source=ingress",
 								"--provider=google", "--domain-filter=$(DOMAIN_NAME)", "--registry=txt",
-								"--txt-owner-id=kctf-cloud-dns-$(CLUSTER_NAME)"},
+								"--txt-owner-id=kctf-cloud-dns"},
 						},
 						{
 							Image: DOCKER_CERTBOT_IMAGE,
